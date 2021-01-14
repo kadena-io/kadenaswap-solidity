d95f0e509cf119630ffbe616ccc4dac529c9f91e
LindaOrtega <linda.ortega2013@gmail.com>
2021-01-14 16:13:07 -0500

    unblock stuart

--- migrations/2_initial_migration.js
+++ migrations/2_initial_migration.js
@@ -1,7 +1,7 @@
-var KadenaBridgeFactory = artifacts.require("KadenaBridgeFactory.sol");
+// var KadenaBridgeFactory = artifacts.require("KadenaBridgeFactory.sol");
 var ToptalToken = artifacts.require("ToptalToken.sol");
 
 module.exports = function(deployer) {
-  deployer.deploy(KadenaBridgeFactory);
+  //deployer.deploy(KadenaBridgeFactory);
   deployer.deploy(ToptalToken);
 };
